9493e53201e07e72f1dafd6483cd99dc09458f3c
AzamovMuhammad <azamov.dragon@gmail.com>
2025-04-08 18:59:14 +0500

    almost finished more page code

--- src/Pages/More.jsx
+++ src/Pages/More.jsx
@@ -1,9 +1,27 @@
 import React from 'react'
+import { FaArrowRight, FaPaperPlane, FaStar, FaComment } from "react-icons/fa";
+
+import "../style/more.css"
+
 
 function More() {
-  return (
-    <div>More</div>
-  )
+    const menuItems = [
+        { text: "Til", icon: <FaArrowRight /> },
+        { text: "Ilovani do‘stlarga ulashing", icon: <FaPaperPlane /> },
+        { text: "Ilovani baholang", icon: <FaStar /> },
+        { text: "Dasturchilar bilan bog‘lanish", icon: <FaComment /> },
+      ];
+    
+      return (
+        <div className="settings-menu">
+          {menuItems.map((item, index) => (
+            <div key={index} className="menu-item">
+              <span className="menu-text">{item.text}</span>
+              <span className="menu-icon">{item.icon}</span>
+            </div>
+          ))}
+        </div>
+      );
 }
 
 export default More
