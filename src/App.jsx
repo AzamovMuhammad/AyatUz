81396202f4f3129728ee4b34961408ec6dbb199e
AzamovMuhammad <azamov.dragon@gmail.com>
2025-04-08 12:40:49 +0500

    home and header connected

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,18 @@ import EntrancePart from "./Pages/entrance";
 import LanguageSelection from "./Pages/LanguageSelection";
 import Next from "./Pages/Next";
 import Header from "./components/header";
+import Home from "./Pages/home";
 
 function App() {
   const [loadingComplete, setLoadingComplete] = useState(false);
 
   return (
     <BrowserRouter>
+    <Header/>
       <Routes>
         <Route path="/" element={!loadingComplete ? <EntrancePart onComplete={() => setLoadingComplete(true)} /> : <Navigate to="/language" />} />
         <Route path="/language" element={<LanguageSelection />} />
-        <Route path="/home" element={<Header />} />
+        <Route path="/home" element={<Home />} />
       </Routes>
     </BrowserRouter>
   );
