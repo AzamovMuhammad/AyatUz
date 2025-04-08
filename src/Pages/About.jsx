df622b0551b9febb84d2d47f33e829b2eeb6d5b0
AzamovMuhammad <azamov.dragon@gmail.com>
2025-04-08 16:44:38 +0500

    finished about page

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -1,12 +1,36 @@
-import React from 'react'
-import '../style/about.css'
+import React from "react";
+import "../style/about.css";
+
+import oldQuron from  '../assets/image/about/quron_old.webp'
+import quron from '../assets/image/about/quron.jpg'
 
 function About() {
   return (
-    <div className='about_page'>
-
+    <div className="about_page">
+        <h1>Qur'on haqida</h1>
+      <div className="firtInfo">
+        <img src={oldQuron} alt="" />
+        <p>
+          Qurʼon (arabcha: القرآن‎‎ oʻqimoq, qiroat qilmoq) – musulmonlarning
+          asosiy muqaddas kitobi. Islom eʼtiqodiga koʻra, Qurʼon vahiy orqali
+          Muhammad paygʻambarga 610—632 yillar davomida nozil qilingan.
+        </p>
+      </div>
+      <div className="secondInfo">
+        <p>
+          Allohning kalomi (Kalomulloh). Qurʼon „Kitob“ (yozuv), „Furqon“ (haq
+          bilan botilning orasini ayiruvchi), „Zikr“ (eslatma), „Tanzil“ (nozil
+          qilingan) kabi nomlar bilan atalib, „Nur“ (yorugʻlik), „Hudo“
+          (hidoyat), „Muborak“ (barakotli), „Mubin“ (ochiq-ravshan), „Bushro“
+          (xushxabar), „Aziz“ (eʼzozlanuvchi), „Majid“ (ulugʻ), „Bashir“
+          (bashorat beruvchi), „Nazir“ (ogohlantiruvchi) kabi soʻzlar bilan
+          sifatlangan. Islom olamida Qurʼon "musʼhaf" nomi bilan ham mashhur.
+          Islom ulamolari Qurʼonning 30 xil nom va sifatlarini sanab oʻtganlar.
+        </p>
+        <img src={quron} alt="" />
+      </div>
     </div>
-  )
+  );
 }
 
-export default About
+export default About;
