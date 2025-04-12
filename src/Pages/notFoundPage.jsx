a08b0215e1c37db3fd084d41ae1f25a0fad35623
AzamovMuhammad <azamov.dragon@gmail.com>
2025-04-12 12:25:12 +0500

    some changes

--- src/Pages/notFoundPage.jsx
+++ src/Pages/notFoundPage.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
+import logo from '../assets/image/brandLogo/ayatLogo.png'
+import '../style/notFoundPage.css'
 
 function NotFoundPage() {
   return (
-    <div>NotFoundPage</div>
+    <div className='notPage'>
+        <div className='notPageImg'>
+            <img src={logo} alt="logo" />
+        </div>
+        <div className='notPageText'>
+            <h1>404</h1>
+            <p>Bu xato!</p>
+            <h3>Ushbu serverda so'ralgan url topilmadi. Biz bilgan hamma narsa shu.</h3>
+        </div>
+    </div>
   )
 }
 
--- src/components/header.jsx
+++ src/components/header.jsx
@@ -2,15 +2,17 @@ import React from "react";
 import { Link } from "react-router-dom";
 import '../style/header.css'
 
+import logoImg from '../assets/image/brandLogo/ayatLogo.png'
+
 function Header() {
   return (
     <div>
       <nav>
         <div className="logo">
-          Ayat<sup>Quiz</sup>
+          <img src={logoImg} alt="logo" />
         </div>
         <ul>
-            <Link className="link" to="/" >Bosh sahifa</Link>
+            <Link className="link" to="/home" >Bosh sahifa</Link>
             <Link className="link" to="/save" >Saqlanganlar</Link>
             <Link className="link" to="/about" >Qur'on haqida</Link>
             <Link className="link" to="/more" >Ko'proq</Link>
