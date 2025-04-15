a97f0cdb2b4aa219c3171170e461d105c8f98047
AzamovMuhammad <azamov.dragon@gmail.com>
2025-04-15 11:29:01 +0500

    about pages fixed

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,29 +1,33 @@
 import React from "react";
-import { Link, NavLink } from "react-router-dom";
-import '../style/header.css'
-
+import { NavLink } from "react-router-dom";
+import '../style/header.css';
 import languages from "../language/language";
+import logoImg from '../assets/image/brandLogo/ayatLogo.png';
 
-import logoImg from '../assets/image/brandLogo/ayatLogo.png'
-
-function Sidebar() {
+function Sidebar({ isOpen, onClose }) {
   const savedLang = localStorage.getItem("selectedLanguage");
-
   const currentLang = languages.find((lang) => lang.code === savedLang);
+
   return (
-    <div>
-      <nav>
-        <div className="logo">
-          <img src={logoImg} alt="logo" />
-        </div>
-        <ul>
-            <NavLink className="link" to="/user/home" >{currentLang?.header.btn1}</NavLink>
-            <NavLink className="link" to="/user/save" >{currentLang?.header.btn2}</NavLink>
-            <NavLink className="link" to="/user/about" >{currentLang?.header.btn3}</NavLink>
-            <NavLink className="link" to="/user/more" >{currentLang?.header.btn4}</NavLink>
-        </ul>
+    <aside className={`sidebar ${isOpen ? "open" : ""}`}>
+      <div className="logo">
+        <img src={logoImg} alt="Logo" />
+      </div>
+      <nav className="nav-links">
+        <NavLink to="/user/home" className="link" onClick={onClose}>
+          {currentLang?.header.btn1}
+        </NavLink>
+        <NavLink to="/user/save" className="link" onClick={onClose}>
+          {currentLang?.header.btn2}
+        </NavLink>
+        <NavLink to="/user/about" className="link" onClick={onClose}>
+          {currentLang?.header.btn3}
+        </NavLink>
+        <NavLink to="/user/more" className="link" onClick={onClose}>
+          {currentLang?.header.btn4}
+        </NavLink>
       </nav>
-    </div>
+    </aside>
   );
 }
 
--- src/components/UserLayout.jsx
+++ src/components/UserLayout.jsx
@@ -1,12 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import { Outlet } from "react-router-dom";
 import Sidebar from "./Sidebar";
+import "../style/userLayout.css";
+
+// ⬇️ react-icons kutubxonasidan hamburger menu
+import { FaBars } from "react-icons/fa";
 
 function UserLayout() {
+  const [sidebarOpen, setSidebarOpen] = useState(false);
+
+  const toggleSidebar = () => setSidebarOpen(!sidebarOpen);
+
   return (
-    <div>
-      <Sidebar />
-      <Outlet />
+    <div className="layout-container">
+      {/* Mobile menu toggle button */}
+      <button className="menu-toggle" onClick={toggleSidebar}>
+        <FaBars size={24} />
+      </button>
+
+      <Sidebar isOpen={sidebarOpen} onClose={() => setSidebarOpen(false)} />
+
+      <main className="main-content">
+        <Outlet />
+      </main>
     </div>
   );
 }
