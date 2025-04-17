71df263130d55d1c5fe1651ddedaac0f054faab2
AzamovMuhammad <azamov.dragon@gmail.com>
2025-04-17 10:41:11 +0500

    finished testType part

--- src/Pages/TestType.jsx
+++ src/Pages/TestType.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
+import '../style/testType.css'
+
+import img from '../assets/image/testType/Quran_2.svg'
 
 function TestType() {
   return (
-    <div>
-        
+    <div className='container'>
+        <img src={img} alt="" />
+        <h1>Iltimos, test turini tanlang</h1>
+        <div className='btns'>
+            <button>Bosqichma-bosqich</button>
+            <button>Butun Qur’on bo’yicha</button>
+            <button>Juz bo’yicha</button>
+            <button>Sura bo’yicha</button>
+        </div>
     </div>
   )
 }
