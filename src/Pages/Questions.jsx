8d9698064cf6b4930cddb402e7cbcdf073273f01
AzamovMuhammad <azamov.dragon@gmail.com>
2025-04-18 11:15:05 +0500

    some changes at question

--- src/Pages/Questions.jsx
+++ src/Pages/Questions.jsx
@@ -1,117 +1,118 @@
-import React, { useState, useRef, useEffect } from 'react';
-import { FaPlay, FaCheckCircle, FaTimesCircle } from 'react-icons/fa';
-import '../style/QuestionCard.css';
-import falaq from '../assets/audio/falaq.mp3'
-import asr from '../assets/audio/asr.mp3'
-import fil from '../assets/audio/fil.mp3'
-import humazah from '../assets/audio/humazah.mp3'
-import kafirun from '../assets/audio/kafirun.mp3'
-import kavsar from '../assets/audio/kavsar.mp3'
-import maun from '../assets/audio/maun.mp3'
-import nas from '../assets/audio/nas.mp3'
-import quraish from '../assets/audio/quraish.mp3'
-import takasur from '../assets/audio/takasur.mp3'
+import React, { useState, useRef, useEffect } from "react";
+import { FaPlay, FaCheckCircle, FaTimesCircle } from "react-icons/fa";
+import { useNavigate } from "react-router-dom";
+import "../style/QuestionCard.css";
+import falaq from "../assets/audio/falaq.mp3";
+import asr from "../assets/audio/asr.mp3";
+import fil from "../assets/audio/fil.mp3";
+import humazah from "../assets/audio/humazah.mp3";
+import kafirun from "../assets/audio/kafirun.mp3";
+import kavsar from "../assets/audio/kavsar.mp3";
+import maun from "../assets/audio/maun.mp3";
+import nas from "../assets/audio/nas.mp3";
+import quraish from "../assets/audio/quraish.mp3";
+import takasur from "../assets/audio/takasur.mp3";
 
 const questions = [
   {
-    text: 'Qiroatdagi oyat qaysi suraga tegishli?',
+    text: "Qiroatdagi oyat qaysi suraga tegishli?",
     audio: falaq,
     options: [
-      { label: 'A', text: 'An-Nas', isCorrect: false },
-      { label: 'B', text: 'Al-Baqarah', isCorrect: false },
-      { label: 'C', text: 'Al-Masad', isCorrect: false },
-      { label: 'D', text: 'Al-Falaq', isCorrect: true },
-    ]
+      { label: "A", text: "An-Nas", isCorrect: false },
+      { label: "B", text: "Al-Baqarah", isCorrect: false },
+      { label: "C", text: "Al-Masad", isCorrect: false },
+      { label: "D", text: "Al-Falaq", isCorrect: true },
+    ],
   },
   {
-    text: 'Bu oyat qaysi suradan olingan?',
+    text: "Bu oyat qaysi suradan olingan?",
     audio: asr,
     options: [
-      { label: 'A', text: 'Al-Fil', isCorrect: false },
-      { label: 'B', text: 'Al-Humazah', isCorrect: false },
-      { label: 'C', text: 'Al-Asr', isCorrect: true },
-      { label: 'D', text: 'An-Nasr', isCorrect: false },
-    ]
+      { label: "A", text: "Al-Fil", isCorrect: false },
+      { label: "B", text: "Al-Humazah", isCorrect: false },
+      { label: "C", text: "Al-Asr", isCorrect: true },
+      { label: "D", text: "An-Nasr", isCorrect: false },
+    ],
   },
   {
-    text: 'Bu oyat qaysi suradan olingan?',
-    audio: fil, 
+    text: "Bu oyat qaysi suradan olingan?",
+    audio: fil,
     options: [
-      { label: 'A', text: 'Al-Fil', isCorrect: true },
-      { label: 'B', text: 'Al-Falaq', isCorrect: false },
-      { label: 'C', text: 'Al-Ikhlas', isCorrect: false },
-      { label: 'D', text: "An-Ma'un", isCorrect: false },
-    ]
+      { label: "A", text: "Al-Fil", isCorrect: true },
+      { label: "B", text: "Al-Falaq", isCorrect: false },
+      { label: "C", text: "Al-Ikhlas", isCorrect: false },
+      { label: "D", text: "An-Ma'un", isCorrect: false },
+    ],
   },
   {
-    text: 'Bu oyat qaysi suradan olingan?',
-    audio: humazah, 
+    text: "Bu oyat qaysi suradan olingan?",
+    audio: humazah,
     options: [
-      { label: 'A', text: 'Al-Fil', isCorrect: false },
-      { label: 'B', text: 'Al-Takathur', isCorrect: false },
-      { label: 'C', text: 'Al-Masad', isCorrect: false },
-      { label: 'D', text: "Al-Humazah", isCorrect: true },
-    ]
+      { label: "A", text: "Al-Fil", isCorrect: false },
+      { label: "B", text: "Al-Takathur", isCorrect: false },
+      { label: "C", text: "Al-Masad", isCorrect: false },
+      { label: "D", text: "Al-Humazah", isCorrect: true },
+    ],
   },
   {
-    text: 'Bu oyat qaysi suradan olingan?',
-    audio: kafirun, 
+    text: "Bu oyat qaysi suradan olingan?",
+    audio: kafirun,
     options: [
-      { label: 'A', text: 'Al-Fil', isCorrect: false },
-      { label: 'B', text: 'Al-Kafirun', isCorrect: true },
-      { label: 'C', text: 'Al-Masad', isCorrect: false },
-      { label: 'D', text: "Al-Humazah", isCorrect: false },
-    ]
+      { label: "A", text: "Al-Fil", isCorrect: false },
+      { label: "B", text: "Al-Kafirun", isCorrect: true },
+      { label: "C", text: "Al-Masad", isCorrect: false },
+      { label: "D", text: "Al-Humazah", isCorrect: false },
+    ],
   },
   {
-    text: 'Bu oyat qaysi suradan olingan?',
-    audio: kavsar, 
+    text: "Bu oyat qaysi suradan olingan?",
+    audio: kavsar,
     options: [
-      { label: 'A', text: 'Al-Kauthar', isCorrect: true },
-      { label: 'B', text: 'Al-Kafirun', isCorrect: false },
-      { label: 'C', text: 'Al-Masad', isCorrect: false },
-      { label: 'D', text: "Al-Humazah", isCorrect: false },
-    ]
+      { label: "A", text: "Al-Kauthar", isCorrect: true },
+      { label: "B", text: "Al-Kafirun", isCorrect: false },
+      { label: "C", text: "Al-Masad", isCorrect: false },
+      { label: "D", text: "Al-Humazah", isCorrect: false },
+    ],
   },
   {
-    text: 'Bu oyat qaysi suradan olingan?',
-    audio: maun, 
+    text: "Bu oyat qaysi suradan olingan?",
+    audio: maun,
     options: [
-      { label: 'A', text: 'Al-Kauthar', isCorrect: false },
-      { label: 'B', text: 'Al-Kafirun', isCorrect: false },
-      { label: 'C', text: 'Al-Masad', isCorrect: false },
-      { label: 'D', text: "Al-Ma'un", isCorrect: true },
-    ]
+      { label: "A", text: "Al-Kauthar", isCorrect: false },
+      { label: "B", text: "Al-Kafirun", isCorrect: false },
+      { label: "C", text: "Al-Masad", isCorrect: false },
+      { label: "D", text: "Al-Ma'un", isCorrect: true },
+    ],
   },
   {
-    text: 'Bu oyat qaysi suradan olingan?',
-    audio: nas, 
+    text: "Bu oyat qaysi suradan olingan?",
+    audio: nas,
     options: [
-      { label: 'A', text: 'Al-Kauthar', isCorrect: false },
-      { label: 'B', text: 'An-Nas', isCorrect: true },
-      { label: 'C', text: 'Al-Masad', isCorrect: false },
-      { label: 'D', text: "Al-Ma'un", isCorrect: false },
-    ]
+      { label: "A", text: "Al-Kauthar", isCorrect: false },
+      { label: "B", text: "An-Nas", isCorrect: true },
+      { label: "C", text: "Al-Masad", isCorrect: false },
+      { label: "D", text: "Al-Ma'un", isCorrect: false },
+    ],
   },
   {
-    text: 'Bu oyat qaysi suradan olingan?',
-    audio: quraish, 
+    text: "Bu oyat qaysi suradan olingan?",
+    audio: quraish,
     options: [
-      { label: 'A', text: 'Quraish', isCorrect: true },
-      { label: 'B', text: 'An-Nas', isCorrect: false },
-      { label: 'C', text: 'Al-Masad', isCorrect: false },
-      { label: 'D', text: "Al-Ma'un", isCorrect: false },
-    ]
+      { label: "A", text: "Quraish", isCorrect: true },
+      { label: "B", text: "An-Nas", isCorrect: false },
+      { label: "C", text: "Al-Masad", isCorrect: false },
+      { label: "D", text: "Al-Ma'un", isCorrect: false },
+    ],
   },
   {
-    text: 'Bu oyat qaysi suradan olingan?',
-    audio: takasur, 
+    text: "Bu oyat qaysi suradan olingan?",
+    audio: takasur,
     options: [
-      { label: 'A', text: 'Quraish', isCorrect: false },
-      { label: 'B', text: 'An-Nas', isCorrect: false },
-      { label: 'C', text: 'Al-Takathur', isCorrect: true },
-      { label: 'D', text: "Al-Ma'un", isCorrect: false },
-    ]
+      { label: "A", text: "Quraish", isCorrect: false },
+      { label: "B", text: "An-Nas", isCorrect: false },
+      { label: "C", text: "Al-Takathur", isCorrect: true },
+      { label: "D", text: "Al-Ma'un", isCorrect: false },
+    ],
   },
 ];
 
@@ -121,7 +122,7 @@ const QuestionCard = () => {
   const [answered, setAnswered] = useState(false);
   const [isPlaying, setIsPlaying] = useState(false);
   const audioRef = useRef(new Audio(questions[0].audio));
-
+  const navigate = useNavigate();
   // 🔁 Savol o‘zgarganda audio yangilansin
   useEffect(() => {
     audioRef.current.pause();
@@ -155,19 +156,49 @@ const QuestionCard = () => {
     setIsPlaying(!isPlaying);
   };
 
+  const questionModal = document.querySelector(".questionModal");
+  function openModal() {
+    questionModal.style.display = "flex";
+  }
+  function closeModal() {
+    questionModal.style.display = "none";
+  }
+  function acseptCloseModal() {
+    navigate('/user/stage')
+  }
+
   const { text, options } = questions[current];
 
   return (
     <div className="card-container">
+      <div className="questionModal">
+        <div className="middle">
+          <h1>
+            Agar chiqsangiz, testni qaytadan boshlashingizga to’g’ri keladi!
+          </h1>
+          <div className="questionBtns">
+            <button onClick={acseptCloseModal} className="rozi">
+              Roziman
+            </button>
+            <button onClick={closeModal} className="bekor">
+              Bekor qilish
+            </button>
+          </div>
+        </div>
+      </div>
       <div className="card">
         <div className="header">
-          <button className="back-btn">←</button>
-          <h2>{current + 1}-savol / {questions.length} savol</h2>
+          <button onClick={openModal} className="back-btn">
+            ←
+          </button>
+          <h2>
+            {current + 1}-savol / {questions.length} savol
+          </h2>
           <p>{text}</p>
         </div>
 
         <div className="audio-play" onClick={toggleAudio}>
-          <FaPlay size={32} style={{ color: isPlaying ? 'green' : 'black' }} />
+          <FaPlay size={32} style={{ color: isPlaying ? "green" : "black" }} />
         </div>
 
         <div className="options">
@@ -178,18 +209,17 @@ const QuestionCard = () => {
             return (
               <button
                 key={idx}
-                className={`option ${isSelected ? 'selected' : ''} ${
+                className={`option ${isSelected ? "selected" : ""} ${
                   answered
                     ? isCorrect
-                      ? 'correct'
+                      ? "correct"
                       : isSelected
-                      ? 'wrong'
-                      : ''
-                    : ''
+                      ? "wrong"
+                      : ""
+                    : ""
                 }`}
                 onClick={() => handleOptionClick(idx)}
-                disabled={answered}
-              >
+                disabled={answered}>
                 <span className="label">{option.label}</span>
                 <span className="text">{option.text}</span>
                 {answered && isSelected && !isCorrect && (
@@ -203,12 +233,8 @@ const QuestionCard = () => {
           })}
         </div>
 
-        <button
-          className="next-btn"
-          onClick={handleNext}
-          disabled={!answered}
-        >
-          {current + 1 === questions.length ? 'Tugatish' : 'Keyingi test →'}
+        <button className="next-btn" onClick={handleNext} disabled={!answered}>
+          {current + 1 === questions.length ? "Tugatish" : "Keyingi test →"}
         </button>
       </div>
     </div>
