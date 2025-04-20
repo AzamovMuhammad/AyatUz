06689495b80b51520b2e1c24ec1c4aa5af8a6174
AzamovMuhammad <azamov.dragon@gmail.com>
2025-04-20 17:39:48 +0500

    some changes

--- src/Pages/entryTest.jsx
+++ src/Pages/entryTest.jsx
@@ -1,22 +1,26 @@
-import React, { useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
-import '../style/entryTest.css';
+import React, { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import "../style/entryTest.css";
 
 function EntryTest() {
   const navigate = useNavigate();
 
   useEffect(() => {
     const timer = setTimeout(() => {
-      navigate('/question');
+      navigate("/question");
     }, 5000); // 5000ms = 5 sekund
 
     return () => clearTimeout(timer); // Komponent unmount bo‘lsa tozalanadi
   }, [navigate]);
 
   return (
-    <div className='container'>
-      <h1>Ayat Quiz</h1>
-      <p>A'uuzu billahi minash shaytonir rojiym. Bismillahir Rohmanir Rohiym!</p>
+    <div className="enrty_container">
+      <div className="middle">
+        <h1>Ayat Quiz</h1>
+        <p>
+          A'uuzu billahi minash shaytonir rojiym. Bismillahir Rohmanir Rohiym!
+        </p>
+      </div>
     </div>
   );
 }
