1769a797405487f370ad81372843f267cc665bb3
AzamovMuhammad <azamov.dragon@gmail.com>
2025-05-02 08:58:12 +0500

    successful connected axios links

--- src/Pages/Stage.jsx
+++ src/Pages/Stage.jsx
@@ -2,59 +2,56 @@ import React, { useEffect, useState } from "react";
 import { FaLock, FaPlay } from "react-icons/fa";
 import "../style/stage.css";
 
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useSearchParams } from "react-router-dom";
 import { FaArrowLeftLong } from "react-icons/fa6";
 import axios from "axios";
 
-// const levels = [
-//   { id: 1, name: "1-bosqich", unlocked: true },
-//   { id: 2, name: "2-bosqich", unlocked: false },
-//   { id: 3, name: "3-bosqich", unlocked: false },
-//   { id: 4, name: "4-bosqich", unlocked: false },
-//   { id: 5, name: "5-bosqich", unlocked: false },
-//   { id: 6, name: "6-bosqich", unlocked: false },
-//   { id: 7, name: "7-bosqich", unlocked: false },
-//   { id: 8, name: "8-bosqich", unlocked: false },
-// ];
-
-
-
-
 function Stage() {
-  const [juz, setJuz] = useState()
+  const [question, setQuestion] = useState([]);
+  const [searchParams] = useSearchParams();
+  const questType = searchParams.get("type");
 
   useEffect(() => {
-    axios.get(`https://api.ayatquiz.com/api/v1/public/web/juz`)
-    .then((res) => {
-      console.log(res.data);
-    })
-  }, [])
-  
+    try {
+      axios
+        .get(`https://api.ayatquiz.com/api/v1/public/web/${questType}/`)
+        .then((res) => {
+          if (questType === 'quiz') {
+            setQuestion(res.data.results);
+          } else {
+            setQuestion(res.data);
+          }
+        });
+    } catch (error) {
+      console.log(error);
+    }
+  }, []);
+
   const navigate = useNavigate();
 
-  // return (
-  //   <div className="level-container">
-  //     <button className="shadow"  onClick={() => {navigate('/user/test')}}>
-  //       <FaArrowLeftLong />
-  //     </button>
-  //     <h1 className="intro-text">Bismillahir rohmanir rohiym, boshladik!</h1>
-  //     {levels.map((level) => (
-  //       <div
-  //         key={level.id}
-  //         className={`level-btn ${level.unlocked ? "unlocked" : "locked"}`}
-  //         onClick={() => {
-  //           if (level.unlocked) {
-  //             navigate("/entry");
-  //           } else {
-  //             alert("Bu bosqich hozircha yopiq.");
-  //           }
-  //         }}>
-  //         <span>{level.name}</span>
-  //         {level.unlocked ? <FaPlay /> : <FaLock />}
-  //       </div>
-  //     ))}
-  //   </div>
-  // );
+  return (
+    <div className="level-container">
+      <button
+        className="shadow"
+        onClick={() => {
+          navigate("/user/home/test");
+        }}>
+        <FaArrowLeftLong />
+      </button>
+      <h1 className="intro-text">Bismillahir rohmanir rohiym, boshladik!</h1>
+      {question.map((level) => (
+        <div
+          key={level.id}
+          className={`level-btn unlocked`}
+          onClick={() => {
+            navigate("/entry");
+          }}>
+          <span>{level.count} - bosqich</span>
+          <FaPlay />
+        </div>
+      ))}
+    </div>
+  );
 }
 
 export default Stage;
--- src/Pages/TestType.jsx
+++ src/Pages/TestType.jsx
@@ -7,44 +7,64 @@ import { FaQuestion } from "react-icons/fa";
 import languages from "../language/language";
 
 function openModal() {
-    const questionModal = document.querySelector('.questionModal')
-    questionModal.style.display = "flex"
+  const questionModal = document.querySelector(".questionModal");
+  questionModal.style.display = "flex";
 }
 function closeModal() {
-    const questionModal = document.querySelector('.questionModal')
-    questionModal.style.display = "none"
+  const questionModal = document.querySelector(".questionModal");
+  questionModal.style.display = "none";
 }
 
 function TestType() {
-    const savedLang = localStorage.getItem("selectedLanguage");
-    const currentLang = languages.find((lang) => lang.code === savedLang);
-    const navigate = useNavigate()
-
+  const savedLang = localStorage.getItem("selectedLanguage");
+  const currentLang = languages.find((lang) => lang.code === savedLang);
+  const navigate = useNavigate();
 
   return (
     <div className="container">
-      <div className="icon">
-        <button className="shadow" onClick={openModal}>
-          <FaQuestion />
-        </button>
-      </div>
+      <button className="shadow" onClick={openModal}>
+        <FaQuestion />
+      </button>
+
       <div className="questionModal">
         <div className="middleDiv">
-            <p className="bosqich">{currentLang?.testTypePart.p1}</p>
-            <p className="butun">{currentLang?.testTypePart.p2}</p>
-            <p className="juz">{currentLang?.testTypePart.p3}</p>
-            <p className="sura">{currentLang?.testTypePart.p4}</p>
-            <h3>{currentLang?.testTypePart.h3}</h3>
-            <button onClick={closeModal}>{currentLang?.testTypePart.closeBtn}</button>
+          <p className="bosqich">{currentLang?.testTypePart.p1}</p>
+          <p className="butun">{currentLang?.testTypePart.p2}</p>
+          <p className="juz">{currentLang?.testTypePart.p3}</p>
+          <p className="sura">{currentLang?.testTypePart.p4}</p>
+          <h3>{currentLang?.testTypePart.h3}</h3>
+          <button onClick={closeModal}>
+            {currentLang?.testTypePart.closeBtn}
+          </button>
         </div>
       </div>
       <img src={img} alt="" />
-      <h1>{currentLang?.testTypePart.h1}</h1>
+      <h1 className="chooseH1">{currentLang?.testTypePart.h1}</h1>
       <div className="btns">
-        <button onClick={() => {navigate(`/user/home/stage`)}}>{currentLang?.testTypePart.btn1}</button>
-        <button onClick={() => {navigate(`/user/home/stage`)}}>{currentLang?.testTypePart.btn2}</button>
-        <button onClick={() => {navigate(`/user/home/stage?type=juz`)}}>{currentLang?.testTypePart.btn3}</button>
-        <button onClick={() => {navigate(`/user/home/stage`)}}>{currentLang?.testTypePart.btn4}</button>
+        <button
+          onClick={() => {
+            navigate(`/user/home/stage?type=quiz`);
+          }}>
+          {currentLang?.testTypePart.btn1}
+        </button>
+        <button
+          onClick={() => {
+            navigate(`/user/home/stage`);
+          }}>
+          {currentLang?.testTypePart.btn2}
+        </button>
+        <button
+          onClick={() => {
+            navigate(`/user/home/stage?type=juz`);
+          }}>
+          {currentLang?.testTypePart.btn3}
+        </button>
+        <button
+          onClick={() => {
+            navigate(`/user/home/stage`);
+          }}>
+          {currentLang?.testTypePart.btn4}
+        </button>
       </div>
     </div>
   );
