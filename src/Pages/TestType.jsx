7e4a61e5b67b60708e9db415139ee0fe251fe80e
AzamovMuhammad <azamov.dragon@gmail.com>
2025-05-13 12:49:49 +0500

    fixed question button in testType page

--- src/Pages/TestType.jsx
+++ src/Pages/TestType.jsx
@@ -19,53 +19,56 @@ function TestType() {
   const savedLang = localStorage.getItem("selectedLanguage");
   const currentLang = languages.find((lang) => lang.code === savedLang);
   const navigate = useNavigate();
-  const {showNotification } = useNotification();
+  const { showNotification } = useNotification();
 
   const handleClickNoti = () => {
-    showNotification("Kechirasiz, sahifa ishlab chiqilmoqda. Tez kunda ishga tushadi.", "info")
-  }
+    showNotification(
+      "Kechirasiz, sahifa ishlab chiqilmoqda. Tez kunda ishga tushadi.",
+      "info"
+    );
+  };
 
   return (
-    <div className="container">
-      <button className="shadow" onClick={openModal}>
-        <FaQuestion />
-      </button>
-
-      <div className="questionModal">
-        <div className="middleDiv">
-          <p className="bosqich">{currentLang?.testTypePart.p1}</p>
-          <p className="butun">{currentLang?.testTypePart.p2}</p>
-          <p className="juz">{currentLang?.testTypePart.p3}</p>
-          <p className="sura">{currentLang?.testTypePart.p4}</p>
-          <h3>{currentLang?.testTypePart.h3}</h3>
-          <button onClick={closeModal}>
-            {currentLang?.testTypePart.closeBtn}
+    <>
+      <div className="shadow-wrapper">
+        <button className="shadow2" onClick={openModal}>
+          <FaQuestion />
+        </button>
+      </div>
+      <div className="container">
+        <div className="questionModal">
+          <div className="middleDiv">
+            <p className="bosqich">{currentLang?.testTypePart.p1}</p>
+            <p className="butun">{currentLang?.testTypePart.p2}</p>
+            <p className="juz">{currentLang?.testTypePart.p3}</p>
+            <p className="sura">{currentLang?.testTypePart.p4}</p>
+            <h3>{currentLang?.testTypePart.h3}</h3>
+            <button onClick={closeModal}>
+              {currentLang?.testTypePart.closeBtn}
+            </button>
+          </div>
+        </div>
+        <img src={img} alt="" />
+        <h1 className="chooseH1">{currentLang?.testTypePart.h1}</h1>
+        <div className="btns">
+          <button
+            onClick={() => {
+              navigate(`/user/home/stage?type=quiz`);
+            }}>
+            {currentLang?.testTypePart.btn1}
+          </button>
+          <button onClick={handleClickNoti}>
+            {currentLang?.testTypePart.btn2}
+          </button>
+          <button onClick={handleClickNoti}>
+            {currentLang?.testTypePart.btn3}
+          </button>
+          <button onClick={handleClickNoti}>
+            {currentLang?.testTypePart.btn4}
           </button>
         </div>
       </div>
-      <img src={img} alt="" />
-      <h1 className="chooseH1">{currentLang?.testTypePart.h1}</h1>
-      <div className="btns">
-        <button
-          onClick={() => {
-            navigate(`/user/home/stage?type=quiz`);
-          }}>
-          {currentLang?.testTypePart.btn1}
-        </button>
-        <button
-          onClick={handleClickNoti}>
-          {currentLang?.testTypePart.btn2}
-        </button>
-        <button
-          onClick={handleClickNoti}>
-          {currentLang?.testTypePart.btn3}
-        </button>
-        <button
-          onClick={handleClickNoti}>
-          {currentLang?.testTypePart.btn4}
-        </button>
-      </div>
-    </div>
+    </>
   );
 }
 
